1f4f3a97c88c914374ec794b461ac1d70d15e6c4
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-02-17 06:53:53 -0800

    correction of 1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,9 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	} else if (last_digit < 6 && last_digit != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		char message[] = "Last digit of %d is %d and is less than 6 and not 0\n";
+
+		printf(message, n, last_digit);
 	}
 
 	return (0);
