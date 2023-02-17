0fe06a0354536e10fd2b4020aacd071ba5162d8d
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-02-17 07:06:27 -0800

    Add 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Enter point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
