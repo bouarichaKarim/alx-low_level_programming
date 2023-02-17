e9323360b6893378ccee1c59db60571a2c7d2925
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-02-17 14:25:35 -0800

    Add 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Enter point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
