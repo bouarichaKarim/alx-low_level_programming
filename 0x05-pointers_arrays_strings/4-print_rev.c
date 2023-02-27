1cbe7398a65ce384547911e2801b1ac372a3a28d
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-02-27 14:27:13 -0800

    Add 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - function that pints a string in reverse
+ * followed by a new line.
+ * @s: parametre char to print.
+ * Return: print char s.
+ */
+void print_rev(char *s)
+{
+	int size = _strlen(*s);
+	int i = 0;
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
