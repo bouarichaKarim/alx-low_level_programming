3e6d69ba8e6e7efa22070648e12f174e1a0fb808
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-03-01 13:35:45 -0800

    correction 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0')
 				break;
-		op = *(s1 = i) + *(s2 + i);
+		op = *(s1 = i) - *(s2 + i);
 		i++;
 	}
 	return (op);
