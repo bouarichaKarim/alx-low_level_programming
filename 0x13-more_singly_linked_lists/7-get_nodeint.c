d189e448a77d714823946949ef5cc21a255fac34
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-04-03 15:24:46 -0700

    Add 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (i = 0; i < index && head != NULL; i++)
 	{
-		head = head->nex;
+		head = head->next;
 	}
 
 	return (head);
