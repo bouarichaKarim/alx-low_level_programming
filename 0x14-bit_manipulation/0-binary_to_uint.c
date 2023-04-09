e45badfd3f6febd9fb8c30b4af6606e107ead13d
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-04-09 09:21:13 -0700

    Add 0-binary_to_int.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,7 @@ unsigned int binary_to_int(const char *b)
 		if (*b != '0' && *b != '1')
 			return (0);
 
-		result = (result << 1) + (*b -'0');
+		result = (result << 1) + (*b - '0');
 		b++;
 	}
 
