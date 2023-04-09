0a2439331dec21b5d8bafe9aa55bc87a5704a330
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-04-09 09:49:55 -0700

    Add 1-print_binary.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_H_
 
 int _putchar(char c);
-unsigned int binary_to_uint(const char *b);
+unsigned int binary_to_uint(const char *b)
+void print_binary(unsigned long int n);;
 
 #endif
