68e16569041efd84fdb30525ac813937d3938d64
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-05-08 13:32:15 -0700

    Update read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,49 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
  *
- * Return: numbers of letters printed. when fails, returns 0.
+ * Return: actual number of letters read and printed, or 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fdesc;
-	ssize_t nreaded, nwroten;
-	char *buffer;
+	ssize_t nreaded, nwritted;
+	char *buf;
 
 	if (!filename)
 		return (0);
 
 	fdesc = open(filename, O_RDONLY);
-
 	if (fdesc == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * (letters));
-	if (!buffer)
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+	{
+		close(fdesc);
 		return (0);
+	}
 
-	nreaded = read(fd, buffer, letters);
-	nwroten = write(STDOUT_FILENO, buffer, nreaded);
+	nreaded = read(fdesc, buf, letters);
+	if (nreaded == -1)
+	{
+		free(buf);
+		close(fdesc);
+		return (0);
+	}
 
+	nwritted = write(STDOUT_FILENO, buf, nreaded);
+	if (nwritted == -1 || nwritted != nreaded)
+	{
+		free(buf);
+		close(fdesc);
+		return (0);
+	}
+
+	free(buf);
 	close(fdesc);
-
-	free(buffer);
-
-	return (nwroten);
+	return (nwritted);
 }
